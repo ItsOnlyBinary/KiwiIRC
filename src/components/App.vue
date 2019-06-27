15f3999493e41f58c0f18b183ea59d3cdd6148b0
ItsOnlyBinary <git@itsonlybinary.com>
2019-06-27 15:19:43 +0100

    update i18next, ip-regex, font-awesome-webpack-4

--- src/components/App.vue
+++ src/components/App.vue
@@ -59,7 +59,7 @@
 <script>
 'kiwi public';
 
-import 'font-awesome-webpack';
+import 'font-awesome-webpack-4';
 import cssVarsPonyfill from 'css-vars-ponyfill';
 import '@/res/globalStyle.css';
 import Tinycon from 'tinycon';
